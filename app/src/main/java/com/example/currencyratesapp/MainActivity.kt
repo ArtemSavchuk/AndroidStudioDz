724fc908ea83672b157718a46828683ddec90fee
sasha <sasha.mikushova@gmail.com>
2024-11-17 23:27:04 +0200

    resolved rotation issue

--- app/src/main/java/com/example/currencyratesapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyratesapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.currencyratesapp
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -14,4 +16,13 @@ class MainActivity : ComponentActivity() {
             CurrencyApp(viewModel)
         }
     }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.d("Orientation", "Landscape")
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Log.d("Orientation", "Portrait")
+        }
+    }
+
 }
